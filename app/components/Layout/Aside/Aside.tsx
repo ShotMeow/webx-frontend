8ae1229edbef021b33aa7c4148d3a4603ba82e92
shotmeow <shotmeow@gmail.com>
2022-11-08 20:06:23 +0300

    Fix Logo Link to Main Page

--- app/components/Layout/Aside/Aside.tsx
+++ app/components/Layout/Aside/Aside.tsx
@@ -3,16 +3,19 @@ import styles from "./Aside.module.css";
 import logo from "../../../assets/images/logo.svg";
 import Image from "next/image";
 import Navigation from "./Navigation/Navigation";
+import Link from "next/link";
 
 const Aside: FC = () => {
     return (
         <aside className={styles.aside}>
-            <h1 className={styles.logo}>
-                <Image src={logo} alt="Логотип" />
-                <span className={styles.name}>
-                    Web<span>X</span>
-                </span>
-            </h1>
+            <Link href="/">
+                <h1 className={styles.logo}>
+                    <Image src={logo} alt="Логотип" />
+                    <span className={styles.name}>
+                        Web<span>X</span>
+                    </span>
+                </h1>
+            </Link>
             <Navigation />
         </aside>
     );
