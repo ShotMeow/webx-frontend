e0b9997196b7bc27676992967dcc3d9cd0509b56
shotmeow <shotmeow@gmail.com>
2022-11-08 18:29:03 +0300

    Small Fixes

--- app/components/Layout/Header/Header.tsx
+++ app/components/Layout/Header/Header.tsx
@@ -4,15 +4,15 @@ import { AiOutlineSearch } from "react-icons/ai";
 import User from "./User/User";
 
 const Header: FC = () => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.search}>
-        <AiOutlineSearch size={25} />
-        <input type="text" placeholder="Поиск..." />
-      </div>
-      <User />
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div className={styles.search}>
+                <AiOutlineSearch size={25} />
+                <input type="text" placeholder="Поиск..." />
+            </div>
+            <User />
+        </header>
+    );
 };
 
 export default Header;
