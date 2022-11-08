ceebbfcd4594c22a8c60ba85ff32b068a508352f
shotmeow <shotmeow@gmail.com>
2022-11-08 19:06:59 +0300

    Add Fonts from @next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from "next/app";
 import "../app/styles/globals.css";
+import { Nunito } from "@next/font/google";
+
+const nunito = Nunito();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <main className={nunito.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
