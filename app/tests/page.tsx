38b0c7933b86a3b179c25bc6446c2097d5dde79b
shotmeow <shotmeow@gmail.com>
2022-11-15 21:24:02 +0300

    Add Tests

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
+import { ITest } from "../../assets/types/api.types";
+import { getTestsCollections } from "../../assets/api/api";
+import Tests from "../../components/Tests/Tests";
 
 const TestsPage: NextPage = () => {
-    return <></>;
+    const [tests, setTests] = useState<ITest[]>([]);
+
+    useEffect(() => {
+        getTestsCollections().then(setTests);
+    }, []);
+    return <main>{tests && <Tests tests={tests} />}</main>;
 };
 
 export default TestsPage;
