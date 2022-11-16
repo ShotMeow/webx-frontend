fa0dfacba0aba596c62ba89fd3aff8c4f26761c7
shotmeow <shotmeow@gmail.com>
2022-11-16 18:26:31 +0300

    Last Fixes before merge

--- app/types/api.types.ts
+++ app/types/api.types.ts
@@ -7,29 +7,3 @@ export interface ITest {
     difficulty: string;
     category: string;
 }
-<<<<<<< HEAD
-=======
-
-export interface IQuestion {
-    id: string;
-    question: string;
-    option: string[];
-    correct_index: number;
-}
-
-export interface ICourse {
-    id: string;
-    title: string;
-    image: string;
-    description: string;
-    developer: string;
-    difficulty: string;
-    rating: number;
-    category: string;
-    info: {
-        about: string;
-        content: string[];
-        require: string;
-    }
-}
->>>>>>> a58668ef10c8025c9a506bcad0b7a011075a1f94
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,8 @@ import "../app/styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-<<<<<<< HEAD
         <Layout>
             <Component {...pageProps} />
         </Layout>
-=======
-        <Component {...pageProps} />
->>>>>>> a58668ef10c8025c9a506bcad0b7a011075a1f94
     );
 }
