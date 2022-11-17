1e4336e5a5af3cf49676b1e614c57f5d7df4c998
Студент СПК <student@spk.local>
2022-11-17 10:11:47 +0300

    Add Carousel to Home Page

--- app/pages/Home/Home.tsx
+++ app/pages/Home/Home.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from "react";
 
+import 'swiper/css';
+import "swiper/css/navigation";
+
 import styles from "./Home.module.scss";
 
-const Home: FC = () => {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+import TestCard from "../../components/Cards/TestCard/TestCard";
+import {ITest} from "../../types/api.types";
+
+interface Props {
+    tests: ITest[]
+}
+
+const Home: FC<Props> = ({tests}) => {
     return (
         <section className={styles.home}>
             <h2>Главная страница</h2>
+            <div className={styles.container}>
+                <h3>Тесты</h3>
+                <Swiper navigation={true} slidesPerView={4} spaceBetween={40} modules={[Navigation]}>
+                    {tests.map((test) => (
+                        <SwiperSlide key={test.id}><TestCard key={test.id} test={test} /></SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </section>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { NextPage } from "next";
 import Home from "../app/pages/Home/Home";
 import Head from "next/head";
+import {ITest} from "../app/types/api.types";
+import {getTestsCollections} from "../app/api/api";
 
 const HomePage: NextPage = () => {
+    const [tests, setTests] = useState<ITest[]>([]);
+
+    useEffect(() => {
+        getTestsCollections().then(setTests);
+    }, []);
+
     return (
         <>
             <Head>
                 <title>WebX - Главная страница</title>
             </Head>
-            <Home />
+            <Home tests={tests} />
         </>
     );
 };
